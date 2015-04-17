52556432a5ad750627e5d6615f0965439b10ccb0
kolinlob <kolinlob@users.noreply.github.com>
2015-04-17 11:37:26 +0300

    deleted <<< HEAD ...

--- Checkers/Checker.cs
+++ Checkers/Checker.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 namespace Checkers
 {
     public class Checker
@@ -17,28 +16,3 @@ namespace Checkers
 
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Checkers
-{
-    public class Checker
-    {
-        private char checkerSymbol;
-        private bool isQueen;
-        private bool isWhite;
-        private int horizontalCoordinate;
-        private int verticalCoordinate;
-
-        //public char CheckerSymbol
-        //{
-        //    get { return CheckerSymbol = this.checkerSymbol; }
-        //    set { checkerSymbol = value; }
-        //}
-    }
-}
->>>>>>> b9fe8532dc5a1c2c377319563375eb3dc27b817c
