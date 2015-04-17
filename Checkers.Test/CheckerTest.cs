638ead23fae7d79ace6262a96431d7d6c248f348
kolinlob <kolinlob@users.noreply.github.com>
2015-04-17 11:41:21 +0300

    deleted <<< HEAD ...

--- Checkers.Test/CheckerTest.cs
+++ Checkers.Test/CheckerTest.cs
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace Checkers.Test
-{
-    [TestClass]
-    public class CheckerTest
-    {
-        [TestMethod]
-        public void CanCreateChecker()
-        {
-            var checker = new Checker();
-
-            Assert.IsNotNull(checker);
-        }
-    }
-}
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Checkers.Test
@@ -36,7 +13,4 @@ namespace Checkers.Test
             Assert.IsNotNull(checker);
         }
     }
-
-
 }
->>>>>>> b9fe8532dc5a1c2c377319563375eb3dc27b817c
