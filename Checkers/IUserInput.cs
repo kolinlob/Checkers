942f3592089b5ef5817b76b4c1e8d0eda8020773
Zhenya21 <pervak_zhenya@i.ua>
2015-05-11 22:17:35 +0300

    IUserInput

---
+++ Checkers/IUserInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    public interface IUserInput
+    {
+    
+        string InputCoordinates();
+
+    }
+}
